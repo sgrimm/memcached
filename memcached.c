f55880e06754ff310b1db0382973ed05b1922919
Steven Grimm <koreth@gmail.com>
2014-02-02 00:18:29 -0800

    Merge reference leak fix in "get" handler

--- memcached.c
+++ memcached.c
@@ -2889,7 +2889,7 @@ static inline void process_get_command(conn *c, token_t *tokens, size_t ntokens,
             if(nkey > KEY_MAX_LENGTH) {
                 out_string(c, "CLIENT_ERROR bad command line format");
                 while (i-- > 0) {
-                    item_remove(*(c->ilist + i));
+                    item_remove(*(c->ilist + i), REFCOUNT_CLIENT);
                 }
                 return;
             }
